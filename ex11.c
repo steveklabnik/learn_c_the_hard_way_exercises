5171f038b11a432b99eb48e86b42aed282d8ed85
Steve Klabnik <steve@steveklabnik.com>
2013-06-05 18:38:56 +0800

    extra credit 1

--- ex11.c
+++ ex11.c
@@ -4,10 +4,10 @@ int main(int argc, char *argv[])
 {
     // go through each string in argv
 
-    int i = 0;
-    while(i < argc) {
-        printf("arg %d: %s\n", i, argv[i]);
-        i++;
+    int i = argc - 1;
+    while(i > 0) {
+        printf("arg %d: %s\n", argc - i, argv[argc - i]);
+        i--;
     }
 
     // let's make our own array of strings
@@ -16,11 +16,10 @@ int main(int argc, char *argv[])
         "Washington", "Texas"
     };
 
-    int num_states = 4;
-    i = 0;  // watch for this
-    while(i < num_states) {
-        printf("state %d: %s\n", i, states[i]);
-        i++;
+    i = 3;  // watch for this
+    while(i > -1) {
+        printf("state %d: %s\n", 3 - i, states[3 - i]);
+        i--;
     }
 
     return 0;
